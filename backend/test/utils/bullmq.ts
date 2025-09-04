39e4fdee9d2573fc76f011ed2289256335514804
Nera26 <106533634+Nera26@users.noreply.github.com>
2025-09-04 22:38:06 +0800

    chore(test): relocate bullmq stub

--- backend/test/utils/bullmq.ts
+++ backend/test/utils/bullmq.ts
@@ -1,13 +1,14 @@
 export class Queue {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars,class-methods-use-this
-  async add(_name: string, _data: unknown, _opts: unknown): Promise<void> {}
+  async add(): Promise<void> {}
 }
 
 export class Worker {
   private readonly processor: () => Promise<void> | void;
+
   constructor(_name: string, processor: () => Promise<void> | void) {
     this.processor = processor;
   }
+
   async waitUntilReady(): Promise<void> {
     await this.processor();
   }
