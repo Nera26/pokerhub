5df9e67d39728c37d67ed1341b35d73edb80403c
Nera26 <106533634+Nera26@users.noreply.github.com>
2025-09-07 21:11:53 +0800

    chore(shared+api): move game action schema to shared

--- backend/src/schemas/game.ts
+++ backend/src/schemas/game.ts
@@ -1,44 +1,4 @@
-import { z } from 'zod';
-
-const BaseAction = z.object({
-  tableId: z.string(),
-  playerId: z.string(),
-});
-
-export const PostBlindActionSchema = BaseAction.extend({
-  type: z.literal('postBlind'),
-  amount: z.number().int().positive(),
-});
-
-export const BetActionSchema = BaseAction.extend({
-  type: z.literal('bet'),
-  amount: z.number().int().positive(),
-});
-
-export const RaiseActionSchema = BaseAction.extend({
-  type: z.literal('raise'),
-  amount: z.number().int().positive(),
-});
-
-export const CallActionSchema = BaseAction.extend({
-  type: z.literal('call'),
-  amount: z.number().int().positive().optional(),
-});
-
-export const CheckActionSchema = BaseAction.extend({
-  type: z.literal('check'),
-});
-
-export const FoldActionSchema = BaseAction.extend({
-  type: z.literal('fold'),
-});
-
-export const NextActionSchema = z.object({
-  type: z.literal('next'),
-  tableId: z.string(),
-});
-
-export const GameActionSchema = z.discriminatedUnion('type', [
+export {
   PostBlindActionSchema,
   BetActionSchema,
   RaiseActionSchema,
@@ -46,7 +6,6 @@ export const GameActionSchema = z.discriminatedUnion('type', [
   CheckActionSchema,
   FoldActionSchema,
   NextActionSchema,
-]);
-
-export type GameAction = z.infer<typeof GameActionSchema>;
-
+  GameActionSchema,
+  type GameAction,
+} from '../../../shared/schemas/game';
--- shared/types.ts
+++ shared/types.ts
@@ -1,4 +1,5 @@
 import { z, ZodError } from 'zod';
+import { GameActionSchema } from './schemas/game';
 
 export { ZodError };
 
@@ -227,61 +228,9 @@ export type FeatureFlag = z.infer<typeof FeatureFlagSchema>;
 export const FeatureFlagsResponseSchema = z.record(z.boolean());
 export type FeatureFlagsResponse = z.infer<typeof FeatureFlagsResponseSchema>;
 
-const BaseAction = z.object({
-  tableId: z.string(),
-  playerId: z.string(),
-});
-
-export const PostBlindActionSchema = BaseAction.extend({
-  type: z.literal('postBlind'),
-  amount: z.number().int().positive(),
-});
-
-export const BetActionSchema = BaseAction.extend({
-  type: z.literal('bet'),
-  amount: z.number().int().positive(),
-});
-
-export const RaiseActionSchema = BaseAction.extend({
-  type: z.literal('raise'),
-  amount: z.number().int().positive(),
-});
-
-export const CallActionSchema = BaseAction.extend({
-  type: z.literal('call'),
-  amount: z.number().int().positive().optional(),
-});
-
-export const CheckActionSchema = BaseAction.extend({
-  type: z.literal('check'),
-});
-
-export const FoldActionSchema = BaseAction.extend({
-  type: z.literal('fold'),
-});
-
-export const NextActionSchema = z.object({
-  type: z.literal('next'),
-  tableId: z.string(),
-});
-
-const GameActionPayloadSchema = z.discriminatedUnion('type', [
-  PostBlindActionSchema,
-  BetActionSchema,
-  RaiseActionSchema,
-  CallActionSchema,
-  CheckActionSchema,
-  FoldActionSchema,
-  NextActionSchema,
-]);
-
-export type GameActionPayload = z.infer<typeof GameActionPayloadSchema>;
-
-export const GameActionSchema = z
-  .object({ version: z.literal('1') })
-  .and(GameActionPayloadSchema);
-
-export type GameAction = z.infer<typeof GameActionSchema>;
+export { GameActionSchema } from './schemas/game';
+export type { GameAction } from './schemas/game';
+export type GameActionPayload = z.infer<typeof GameActionSchema>;
 
 export const GameStatePlayerSchema = z.object({
   id: z.string(),
