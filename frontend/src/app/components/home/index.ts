aafff3f2cd83b932de033ef51786f69786804473
Nera26 <106533634+Nera26@users.noreply.github.com>
2025-09-09 21:00:23 +0800

    chore(frontend): remove unused GameType export

--- frontend/src/app/components/home/index.ts
+++ frontend/src/app/components/home/index.ts
@@ -1,4 +1,3 @@
 export { default as TopCTAs } from './TopCTAs';
 export { default as GameTabs } from './GameTabs';
-export type { GameType } from '@shared/types';
 export { default as HomeLoadingSkeleton } from './HomeLoadingSkeleton';
