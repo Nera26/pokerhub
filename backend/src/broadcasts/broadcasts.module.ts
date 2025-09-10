d13a7f4ead88dbf151a008967a13be32e43a8745
Nera26 <106533634+Nera26@users.noreply.github.com>
2025-09-10 16:38:02 +0800

    feat(api): add broadcast template entity migration

--- backend/src/broadcasts/broadcasts.module.ts
+++ backend/src/broadcasts/broadcasts.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BroadcastEntity } from '../database/entities/broadcast.entity';
+import { BroadcastTemplateEntity } from '../database/entities/broadcast-template.entity';
 import { BroadcastsService } from './broadcasts.service';
 import { BroadcastsController } from './broadcasts.controller';
 import { BroadcastTemplatesController } from './templates.controller';
 import { BroadcastTypesController } from './types.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BroadcastEntity])],
+  imports: [TypeOrmModule.forFeature([BroadcastEntity, BroadcastTemplateEntity])],
   providers: [BroadcastsService],
   controllers: [BroadcastsController, BroadcastTemplatesController, BroadcastTypesController],
 })
