ce2b26c183a0b81c0e52fad753e3b71bf79d319a
Nera26 <106533634+Nera26@users.noreply.github.com>
2025-09-16 14:33:05 +0800

    chore(ui): remove ThemeColor module

--- frontend/src/app/components/ui/__tests__/colors.test.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeColor, themeColorMap } from '../colors';
-
-describe('themeColorMap', () => {
-  it('maps each ThemeColor to a Tailwind class', () => {
-    Object.values(ThemeColor).forEach((color) => {
-      expect(themeColorMap[color].startsWith('bg-')).toBe(true);
-    });
-  });
-});
